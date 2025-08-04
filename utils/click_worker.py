d611f4a652e08e15056df32ae0d79f28d49bc28d
Twaish <axiomeverything@gmail.com>
2025-08-04 21:28:16 +0200

    Add ClickWorker
    - Add ClickWorker using Qt signals

---
+++ utils/click_worker.py
@@ -0,0 +1,56 @@
+import time
+import threading
+from utils.window_utils import *
+from PyQt6.QtCore import pyqtSignal, QObject
+
+DEFAULT_INTERVAL = 1.0
+SLEEP_DURATION = 0.01
+
+class ClickWorker(QObject):
+  status_update = pyqtSignal(str)
+  finished = pyqtSignal()
+
+  def __init__(self):
+    super().__init__()
+    self._is_pressing = False
+    self._hwnd = None
+    self._interval = DEFAULT_INTERVAL
+    self._stop_event = threading.Event()
+
+  def start_pressing(self, hwnd, interval):
+    if not hwnd:
+      return False
+    
+    self._hwnd = hwnd
+    try:
+      self._interval = float(interval)
+      if self._interval <= 0:
+        raise ValueError("Interval must be positive")
+    except Exception as e:
+      self.status_update.emit(f"Invalid interval: {e}")
+      return False
+    
+    self._stop_event.clear()
+    self._is_pressing = True
+    threading.Thread(target=self._press_loop, daemon=True).start()
+    self.status_update.emit("Started presssing.")
+    return True
+
+  def stop_pressing(self):
+    self._is_pressing = False
+    self._stop_event.set()
+    self.status_update.emit("Stopping...")
+  
+  def _press_loop(self):
+    while self._is_pressing and not self._stop_event.is_set():
+      hwnd = self._hwnd
+      if is_window_focused(hwnd) and is_cursor_in_window(hwnd) and is_mouse_button_pressed():
+        time.sleep(SLEEP_DURATION)
+        continue
+      position = (318, 492)
+      click_position(hwnd, position[0], position[1])
+      if self._stop_event.wait(self._interval):
+        break
+    self._is_pressing = False
+    self.status_update.emit("Ended pressing thread.")
+    self.finished.emit()
